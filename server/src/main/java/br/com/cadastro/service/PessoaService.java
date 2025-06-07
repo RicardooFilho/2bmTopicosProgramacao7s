1f2fd9c313e60f8b6a508761f625ead00414e1f4
Ricardo Tagami <ricardo-tagami@hotmail.com>
2025-06-07 19:59:46 -0300

    adicionando service de pessoa

---
+++ server/src/main/java/br/com/cadastro/service/PessoaService.java
@@ -0,0 +1,12 @@
+package br.com.cadastro.service;
+
+import br.com.cadastro.repository.PessoaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PessoaService {
+
+    private final PessoaRepository repository;
+}
