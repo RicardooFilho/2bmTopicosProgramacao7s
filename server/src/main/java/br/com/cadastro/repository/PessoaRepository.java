52b850b3350ccf1e45c967da0713a5a5c186adc4
Ricardo Tagami <ricardo-tagami@hotmail.com>
2025-06-07 19:58:45 -0300

    adicionando repository de pessoa

---
+++ server/src/main/java/br/com/cadastro/repository/PessoaRepository.java
@@ -0,0 +1,9 @@
+package br.com.cadastro.repository;
+
+import br.com.cadastro.domain.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+}
