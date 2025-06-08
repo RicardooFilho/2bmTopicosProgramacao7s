61c6b9c18ff5a90690dede5f72b97e61e181efb5
Ricardo Tagami <ricardo-tagami@hotmail.com>
2025-06-07 23:13:10 -0300

    desenvolvendo a service e controller

--- server/src/main/java/br/com/cadastro/controller/PessoaController.java
+++ server/src/main/java/br/com/cadastro/controller/PessoaController.java
@@ -1,9 +1,14 @@
 package br.com.cadastro.controller;
 
+import br.com.cadastro.domain.Pessoa;
 import br.com.cadastro.service.PessoaService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/pessoas")
@@ -11,4 +16,32 @@ import org.springframework.web.bind.annotation.RestController;
 public class PessoaController {
 
     private final PessoaService service;
+
+    @GetMapping
+    public ResponseEntity<List<Pessoa>> findAll() {
+
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Pessoa> create(@RequestBody @Valid Pessoa newPessoa) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(newPessoa));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable(value = "id") Long id, @RequestBody @Valid Pessoa newPessoa) {
+
+        service.update(id, newPessoa);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<List<Pessoa>> findAll(@PathVariable(value = "id") Long id) {
+
+        service.delete(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
--- server/src/main/java/br/com/cadastro/service/PessoaService.java
+++ server/src/main/java/br/com/cadastro/service/PessoaService.java
@@ -1,12 +1,34 @@
 package br.com.cadastro.service;
 
+import br.com.cadastro.domain.Pessoa;
 import br.com.cadastro.repository.PessoaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class PessoaService {
 
     private final PessoaRepository repository;
+
+    public List<Pessoa> findAll() {
+        return repository.findAll();
+    }
+
+    public Pessoa create(Pessoa newPessoa) {
+        return repository.save(newPessoa);
+    }
+
+    public void update(Long id, Pessoa newPessoa) {
+        repository.findById(id)
+                .map(pessoa -> repository.save(newPessoa))
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
